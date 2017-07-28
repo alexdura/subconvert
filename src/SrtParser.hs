e27e82161723d63b6ac541fe3388054c290c1ab0
alexdura <alexdura@gmail.com>
2017-07-28 12:40:18 +0200

    Somewhat working parser.

--- src/SrtParser.hs
+++ src/SrtParser.hs
@@ -2,9 +2,9 @@ module SrtParser where
 
 import qualified SubContainer as SC
 import Text.ParserCombinators.Parsec
---import Text.Parsec.Prim (modifyState)
 import Text.Parsec.Char
 import Data.Char
+import Data.List
 
 makeDefaultSubtitle :: Int -> Int -> String -> SC.Subtitle
 makeDefaultSubtitle start stop text =
@@ -48,35 +48,43 @@ data TextTree = TextTag TextTree (SrtState -> SrtState)
               | LeafText String
               | TextNode [TextTree]
 
+dump :: TextTree -> String
+dump = dumpi 0
+dumpi :: Int -> TextTree -> String
+dumpi n (LeafText s) = replicate n ' ' ++  s ++ "\n"
+dumpi n (TextTag t _) = replicate n ' ' ++ "t:\n" ++ dumpi (n + 1) t
+dumpi n (TextNode ns) = foldl' (\l -> \r -> l ++ (dumpi n r)) "" ns
+
 defaultState = SrtState False False False ""
 
-boldOn = try (string "<b>") <|> string "{b}"
-boldOff = try (string "</b>") <|> string "{/b}"
+boldOn = string "<b>" <|> string "{b}"
+boldOff = string "</b>" <|> string "{/b}"
 
-pbold = (\t -> TextTag t b) <$> (between boldOn boldOff parseText)
+pbold = (\t -> TextTag t b) <$> (between (try boldOn) (try boldOff) parseText)
 
-italicOn = try (string "<i>") <|> string "{i}"
-italicOff = try (string "</i>") <|> string "{/i}"
+italicOn = string "<i>" <|> string "{i}"
+italicOff = string "</i>" <|> string "{/i}"
 
-pitalic = (\t -> TextTag t i) <$> (between italicOn italicOff parseText)
+pitalic = (\t -> TextTag t i) <$> (between (try italicOn) (try italicOff) parseText)
 
-underlineOn = try (string "<u>") <|> string "{u}"
-underlineOff = try (string "</u>") <|> string "{/u}"
+underlineOn = string "<u>" <|> string "{u}"
+underlineOff = string "</u>" <|> string "{/u}"
 
-punderline = (\t -> TextTag t u) <$> (between underlineOn underlineOff parseText)
+punderline = (\t -> TextTag t u) <$>
+             (between (try underlineOn) (try underlineOff) parseText)
 
 colorOn = do {
   string "<font";
   spaces;
   string "color=\"";
   color <- manyTill anyChar (try (char '"'));
-  string "\">";
+  string ">";
   return color;
   }
 
-colorOff = string "</color>"
+colorOff = string "</font>"
 pcolor = do {
-  cc <- colorOn;
+  cc <- try colorOn;
   t <- parseText;
   colorOff;
   return (TextTag t (c cc));
@@ -84,20 +92,11 @@ pcolor = do {
 
 emptyLine = do {
   endOfLine;
-  spaces;
   endOfLine;
   return "";
   }
 
-plain = do {
-  t <- manyTill anyChar (try emptyLine <|>
-                         try boldOn <|>
-                         try italicOn <|>
-                         try colorOn <|>
-                         try underlineOn <|>
-                         try colorOn);
-  return (LeafText t);
-  }
+plain = (\t -> LeafText t) <$> many1 (noneOf "<>")
 
 formatOrPlain = (try pbold) <|>
                 (try pitalic) <|>
@@ -105,11 +104,29 @@ formatOrPlain = (try pbold) <|>
                 (try pcolor) <|>
                 plain
 
-parseText = (\t -> TextNode t) <$> many1 formatOrPlain
+parseText = (\t -> TextNode t) <$> many formatOrPlain
+
+dumpResult (Right t) = dump t
+dumpResult (Left e) = show e
 
 test1 = runParser parseTimeStamp defaultState "" "10:11:39,100"
+test2 = dumpResult $ runParser plain () "" "abcd abcsd <b>x</b> ahs\n\n"
+test20 = dumpResult $ runParser plain () "" "abcd abcsd"
+test21 = dumpResult $ runParser plain () "" "abcd abcsd  ahs\n\n"
+test3 = runParser emptyLine () "" "\n\n"
+test4 = dumpResult $ runParser pbold () "" "<b><i>abcd</i><b></b></b>\n\n"
+test40 = dumpResult $ runParser pcolor () "" "<font color=\"ABC\">xxx </font>"
+test6 = dumpResult $ runParser parseText () ""
+        "Hello <b>my name is</b>\
+        \What is yours? <font color=\"XXX\">\
+        \My color is </font>\n\
+        \\n"
+
 -- test2 = runParser boldOn defaultState "" "b{b}"
 -- test3 = runParser boldOn defaultState "" "{b}"
 -- test4 = runParser boldOff defaultState "" "{b}"
-test5 = runParser parseText () ""
-        "<b> HEllo <i> JKJKJK </i> xxx </b>"
+test5 = let r = runParser parseText () ""
+                "xxx"
+        in case r of
+            Right t -> dump t
+            Left e -> "Error"
